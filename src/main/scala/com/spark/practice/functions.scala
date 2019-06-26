b2b1b723b3555cb9e5af3a3e53c97a2c99c87a2a
Abdul <mhab0116@gmail.com>
2019-06-26 22:39:35 +0530

    functions update

--- src/main/scala/com/spark/practice/functions.scala
+++ src/main/scala/com/spark/practice/functions.scala
@@ -17,7 +17,7 @@ object functions {
     val result= anonyFunc(2,square)
     println(result)
 
-    // Function with option parameter
+println("********************* Options in scala **************")
     /* Scala option
     use the Option class when returning a value from a function that can be null.
     Simply stated, instead of returning one object when a function succeeds and null when it fails,
@@ -57,7 +57,7 @@ object functions {
     Ref: https://alvinalexander.com/scala/scala-2.10-implicit-class-example
          https://alvinalexander.com/scala/scala-implicit-method-arguments-fields-example
      */
-    println("********************* IMPLICIT method argument and field **************")
+println("********************* IMPLICIT method argument and field **************")
     // Define a method that takes implicit string
     def maldiv(implicit s:String)={
       println(s"im going to visit "+s)
@@ -81,14 +81,14 @@ object functions {
     // OP : im going to visit paradise island
 
     // Declare another Implicit string field
-    implicit val expensive = "Fun Island"
-
-    maldiv
+//    implicit val expensive = "Fun Island"
+//
+//    maldiv
     //    Error:(83, 5) ambiguous implicit values:
     //    both value expensive of type String
     //    and value place of type String
 
-    println("********************* IMPLICIT method argument and field **************")
+println("********************* IMPLICIT class **************")
 
     implicit class StringImprovements(s: String) {
      def increment = s.map(c => (c + 1).toChar)
@@ -96,6 +96,43 @@ object functions {
 
     val result1 = "HAL".increment
     println(result1)
+
+println("********************* TYPED Functions ****************")
+    /*
+    Typed functions -> Functions whose parameter can take any datatype
+
+    Example :  lets say i need a method which should take both Int & String type like below.
+               But Declaring two methods with same name will throw error.
+               So the solution here is TYPED Function
+
+    def honeyMoon(place:String): Unit ={
+      println("im going to "+place)
+    }
+
+    def honeyMoon(budget:Int): Unit ={
+      println("My honeymoon budget is"+budget)
+    }
+
+    honeyMoon(10000)
+    honeyMoon("maldives")
+    */
+
+    def honeyMoon[T](plan:T): Unit ={
+
+      plan match {
+        case d : String => println("im going to "+d)
+        case d : Int => println("my honeymoon budget is "+d)
+        case _ =>  println("unsupported discount type")
+      }
+
+    }
+    //How to call a function which has typed parameters
+    honeyMoon("maldives")
+    honeyMoon(100000)
+    // o/p:
+    // ********************* TYPED Functions ****************
+    //      im going to maldives
+    //      my honeymoon budget is 100000
 
 
   }
